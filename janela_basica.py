1a8bbd386dff4e8884726194c8229a736cb5d53e
Danvb112 <danielbonasser@gmail.com>
2019-05-28 22:11:40 -0300

    Tkinter

---
+++ janela_basica.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+janela = tk.Tk()
+
+janela.title("Janela Principal")
+
+janela["bg"] = "blue"
+
+#LxA+E+T
+#300x300+100+100
+
+janela.geometry("300x300+100+100")
+
+
+janela.mainloop()
+
+
+
